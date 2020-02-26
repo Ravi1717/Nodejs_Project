c775354489a17a54a3d0052d83dbfd128ae1e513
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-02-26 17:43:14 +0530

    Build a RestFul Api with Nodejs,Express & MongoDB

---
+++ rest-api/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
+//var env = require("dotenv").config({ path: "DB_CONNECTION" });
+
+const connectString =
+  "mongodb+srv://ravi:ravi@12345@cluster0-lvl1j.mongodb.net/test?retryWrites=true&w=majority";
+
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require("./routes/posts");
+
+app.use("/posts", postsRoute);
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.send("we are on home");
+});
+
+//Connect to DB
+
+mongoose.connect(
+  connectString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to DB");
+  }
+);
+
+//how do start listening to server
+
+app.listen(3000);
