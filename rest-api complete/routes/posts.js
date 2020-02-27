244fe59d32d6bf5f63de84573b7523a5b927e605
Ravi Verma <46916221+Ravi1717@users.noreply.github.com>
2020-02-27 14:46:36 +0530

    Rest Api using express,mongodb and nodejs

---
+++ rest-api complete/routes/posts.js
@@ -0,0 +1,69 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Post = require("./models/Post");
+
+//ROUTES
+
+///GET BACK ALL THE POSTS
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//SUBMITS A POST
+router.post("/", async (req, res) => {
+  //console.log(req.body);
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//SPECIFIC POST
+router.get("/:postId", async (req, res) => {
+  //console.log(req.params.postId);
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//DELETE A SPECIFIC POST
+
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removePost = await Post.remove({ _id: req.params.postId });
+    res.json(removePost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//UPDATE A POST
+router.patch("/:postId", async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
